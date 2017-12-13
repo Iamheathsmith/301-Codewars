6c6a9c656aa458e786eeeb40a2e5c7b765c1ea30
heath smith <heathsmith73@gmail.com>
2017-12-12 17:22:00 -0800

    done with highest

---
+++ highest-scoring-word/solution.js
@@ -0,0 +1,17 @@
+function high(x){
+  var words = x.split(' '),
+    wordVal = 0,
+    maxValue = '';
+  for(let i = 0; i < words.length; i++){
+    var singleWord = words[i],
+      valOfWord = 0;
+    for(let p = 0; p < singleWord.length; p++){
+      valOfWord += (singleWord.charCodeAt(p) - 96);
+    }
+    if(valOfWord > wordVal){
+      wordVal = valOfWord;
+      maxValue = singleWord;
+    }
+  }
+  return maxValue;
+}
